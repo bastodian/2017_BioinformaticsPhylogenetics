b8b73efa671a9327fc69e6a8dd28f6af19b5d8f6
bastodian <bastodian@gmail.com>
2017-09-24 22:15:28 +1000

    added Fasta Parser with Biopython

---
+++ Week_6/2_Fasta_Parser/Fasta_Parser_BioPython.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+'''
+    Script to sort a mixed Fasta file containing nucleotide and protein sequences
+    using BioPython
+'''
+
+from sys import argv
+from sys import stdout
+from sys import stderr
+from Bio import SeqIO, Seq
+
+import warnings
+from Bio import BiopythonWarning
+warnings.simplefilter('ignore', BiopythonWarning)
+
+InFile = argv[1]
+
+with open(InFile, 'rU') as FastaFile:
+    for Record in SeqIO.parse(FastaFile, "fasta"):
+        try:
+            Seq.Seq(str(Record.seq)).translate()
+            stdout.write('>%s\n%s\n' % (str(Record.description), \
+                    Seq.Seq(str(Record.seq))))
+        except Exception:
+            stderr.write('>%s\n%s\n' % (str(Record.description), \
+                    Seq.Seq(str(Record.seq))))
